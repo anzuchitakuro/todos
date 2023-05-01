3b96be3582e13032297bcf9215b8854978d28690
Takuro Anzuchi <dfre0821@gmail.com>
2023-05-01 13:44:19 +0900

    lint check

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: Home(),
     );
+  }
+}
+
+class Home extends HookConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Text('hello world');
   }
 }
 
